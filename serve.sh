fb50cab4b80014ddc3b528f12159d4b0ae623538
Eddie Zhang <bestenze@gmail.com>
2018-04-27 14:26:25 -0400

    change

--- serve.sh
+++ serve.sh
@@ -11,7 +11,7 @@ gitbook install && gitbook build
 git checkout gh-pages
 
 # pull the latest updates
-git pull origin gh-pages --rebase
+#git pull origin gh-pages --rebase
 
 # copy the static site files into the current directory.
 cp -R _book/* .
